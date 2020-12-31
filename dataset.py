50db4d16d1c251b4bd678cd30c1e7fa01441bc22
mdawess <64979871+mdawess@users.noreply.github.com>
2020-12-30 19:48:02 -0500

    fixed the training and testing data

--- dataset.py
+++ dataset.py
@@ -11,10 +11,25 @@ def trainingData(folder):
     i = 0
     for filename in listdir(folder):
         # load image
-        img_data = image.imread(
-            folder + '/' + 'mask' + str(i) + '.jpg')
+        img_data = image.imread(folder + '/' + filename)
         # store loaded image
         trainingImages.append(img_data)
         i += 1
 
     return np.array(trainingImages)
+
+
+def trainingLabels(folder):
+    """Returns the labels for each of the images in the training dataset to be used
+    as the dependent variable.
+    """
+    labels = []
+    i = 0
+    for filename in listdir(folder):
+        # Gets each filename from folder and removes the # at the end
+        parsed = ''.join([i for i in filename if not i.isdigit()])
+        # Appends the filename to the list excluding .jpg
+        labels.append(parsed[:-4])
+        i += 1
+
+    return np.array(labels)
--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow import keras
 import numpy as np
-from dataset import trainingData
+from dataset import trainingData, trainingLabels
 
-# Loading in the training and testing data
-ds_train = (trainingData("Mask_Dataset"))
-ds_test = (trainingData("Testing_Set"))
+# Loading in the training and testing data (x and y var for each)
+ds_train = trainingData("training_set")  # x variable
+ds_test = trainingData("testing_set")
+training_labels = trainingLabels("training_set")  # y variable
+testing_labels = trainingLabels("testing_set")
 
 # Shape
 # Training - (966, 1024, 1024, 3)
@@ -15,22 +17,21 @@ ds_test = (trainingData("Testing_Set"))
 # Converting to float values from integers
 ds_train = ds_train.astype('float32')
 ds_test = ds_test.astype('float32')
-print(ds_train.shape, ds_test.shape)
+# print(ds_train.shape, ds_test.shape)
 
 # Scaling values to be between 0 and 1 for performance
 ds_train /= 255
 ds_test /= 255
 
 # Defining the model
-model = keras.Sequential({
-    keras.layers.Dense(512, input_dim=1024, activation='relu'),
-    keras.layers.Dense(256, activation='relu'),
-    keras.layers.Dense(1, activation='softmax')
-})
+model = keras.models.Sequential()
+model.add(keras.layers.Dense(512, input_dim=1024, activation='relu',))
+model.add(keras.layers.Dense(256, activation='relu'))
+model.add(keras.layers.Dense(2, activation='sigmoid'))
 
-# Defining the parameters of the model
+# # Defining the parameters of the model
 model.compile(optimizer='adam',
-              loss=keras.losses.sparse_categorical_crossentropy(),
+              loss="binary_crossentropy",
               metrics=['accuracy'])
 
 # Fitting the model
